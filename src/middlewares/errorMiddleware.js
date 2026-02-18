13533c364425ee9d6b331202caef0bd0f37e0b2f
abhi230104001 <abhishek2304001@gmail.com>
2026-02-19 01:16:21 +0530

    fix issue

--- src/middlewares/errorMiddleware.js
+++ src/middlewares/errorMiddleware.js
@@ -1,6 +1,6 @@
 const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-    console.error(err.stack); // Log error stack to console
+    console.error(err.stack); 
     res.status(statusCode);
     res.json({
         message: err.message,
